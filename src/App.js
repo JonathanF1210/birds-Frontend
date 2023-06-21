d44894d44fb4f0a6368402cec2c2c5fc36fe6b10
JonathanF1210 <jonathanf1210@gmail.com>
2023-06-20 23:14:12 -0500

    cleaned up app.js and allBirds.js. Added a search bar to allBirds.js

--- src/App.js
+++ src/App.js
@@ -1,46 +1,21 @@
 import './App.css';
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import AllBirds from './components/allBirds/allBirds';
-import SingleBird from './components/singleBirds/singleBird.js';
-
+import SingleBird from './components/singleBirds/singleBird';
 
 function App() {
-  const [birds, setBirds] = useState([]); // State to store the fetched data
-
-  const fetchData = () => {
-    fetch("http://localhost:8080/api/v1/birds")
-      .then(function(response) {
-        if (response.ok) {
-          return response.json(); // Parse the response as JSON
-        } else {
-          throw new Error("Failed to fetch birds");
-        }
-      })
-      .then(function(data) {
-        setBirds(data); // Update the state with the fetched data
-      })
-      .catch(function(error) {
-        alert("An error occurred"); // Show error message
-        console.error(error);
-      });
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
   return (
     <div className="App">
       <Router>
-      <Routes>
-        <Route path="/allBirds" element={<AllBirds birds={birds}/>} />
-        <Route path="/bird/:name" element={<SingleBird />} />
-      </Routes>
-
+        <Routes>
+          <Route path="/allBirds" element={<AllBirds />} />
+          <Route path="/bird/:name" element={<SingleBird />} />
+        </Routes>
       </Router>
     </div>
   );
 }
 
 export default App;
+
--- src/components/allBirds/allBirds.js
+++ src/components/allBirds/allBirds.js
@@ -1,12 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+
 import './allBirds.css';
 
-const allBirds = ({ birds }) => {
+const AllBirds = () => {
+  const [birds, setBirds] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+    // Fetch the bird data
+    fetch("http://localhost:8080/api/v1/birds")
+      .then(function(response) {
+        if (response.ok) {
+          return response.json(); // Parse the response as JSON
+        } else {
+          throw new Error("Failed to fetch birds");
+        }
+      })
+      .then(function(data) {
+        setBirds(data); // Update the state with the fetched data
+      })
+      .catch(function(error) {
+        alert("An error occurred"); // Show error message
+        console.error(error);
+      });
+  }, []);
+
+  // Filter the birds based on the search term
+  const filteredBirds = birds.filter((bird) =>
+    bird.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <div className="birds-container">
       <h1>All Birds</h1>
-      {birds.map((bird) => (
+      <input
+        type="text"
+        placeholder="Search birds..."
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+      {filteredBirds.map((bird) => (
         <div key={bird.id} className="bird-card">
           <Link to={`/bird/${bird.name}`}>
             <strong>{bird.name}</strong>
@@ -21,6 +55,6 @@ const allBirds = ({ birds }) => {
   );
 };
 
+export default AllBirds;
 
 
-export default allBirds;
